f5b0f7dbf245a69a1b22754940ee69c8aa457232
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2023-02-14 16:00:54 +0900

    GenImageViewer の実行ポリシーを RemoteSigned から Bypass に変更。

--- Tool/GenImageViewer/GenImageViewer.bat
+++ Tool/GenImageViewer/GenImageViewer.bat
@@ -1,2 +1,2 @@
 @echo off
-PowerShell -Version 5.1 -ExecutionPolicy RemoteSigned -File "%~dp0GenImageViewer.ps1" "%~1"
+PowerShell -Version 5.1 -ExecutionPolicy Bypass -File "%~dp0GenImageViewer.ps1" "%~1"
